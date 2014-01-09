a8375a693a6502f1d4aa21c7e5e25d338d85b01d
Antti Laakso <antti.laakso@intel.com>
2014-01-09 09:00:33 +0200

    Create some payload for stub parameters.
    Do this by extending parameter structures with existing datatypes
    from CSS.
    
    Signed-off-by: Antti Laakso <antti.laakso@intel.com>
    Reviewed-by: Kai Vehmanen <kai.vehmanen@intel.com>

--- drivers/media/platform/atomisp/psysstub.h
+++ drivers/media/platform/atomisp/psysstub.h
@@ -2,42 +2,44 @@
  * Copyright (c) 2013 Intel Corporation. All Rights Reserved.
  */
 
+#include "psys_ia_css_types.h"
+
 enum {
 	ATOMISP_PSYS_STUB_PREVIEW = 1,
 };
 
-/** Unsigned fixed point value, 0 integer bits, 16 fractional bits */
-#define u0_16 unsigned int
+struct atomisp_rect {
+	uint32_t left;
+	uint32_t top;
+	uint32_t width;
+	uint32_t height;
+};
 
-/** Optical black mode.
+/**
+ * struct atomisp_frame_info - describe a frame
+ * @width:		frame width in pixels
+ * @height:		frame height in pixels
+ * @bytesperline:	line length in bytes
+ * @frame_format:	frame format
+ * @bayer_order:	bayer order
+ * @bit_depth:		bits per pixel
+ * @rect:		area of interest
+ *
+ * Describes a videoframe.
  */
-enum sh_css_ob_mode {
-	sh_css_ob_mode_none,
-	sh_css_ob_mode_fixed,
-	sh_css_ob_mode_raster
-};
-
-struct sh_css_ob_config {
-	enum sh_css_ob_mode mode; /**< Mode (Fixed / Raster) */
-	u0_16 level_gr;    /**< Black level for GR pixels */
-	u0_16 level_r;     /**< Black level for R pixels */
-	u0_16 level_b;     /**< Black level for B pixels */
-	u0_16 level_gb;    /**< Black level for GB pixels */
-	unsigned short start_position; /**< Start position of OB area (used for
-				      raster mode only). Valid range is [0..63]. */
-	unsigned short end_position;  /**< End position of OB area (used for
-				      raster mode only).
-				      Valid range is [start_pos..64]. */
-};
-
-struct preview_params {
-	struct sh_css_ob_config ob;
+struct atomisp_frame_info {
+	uint32_t width;
+	uint32_t height;
+	uint32_t bytesperline;
+	uint32_t frame_format; /* enum ia_css_frame_format */
+	uint32_t bayer_order; /* enum ia_css_bayer_order, valid for raw only */
+	uint32_t bit_depth;
+	struct atomisp_rect rect;
+	uint32_t reserved[2];
 };
 
 struct psysparam {
-	uint32_t len;
-	union {
-		struct preview_params params;
-		uint8_t* buffer;
-	} m;
+	struct atomisp_frame_info input;	/* describe input frame */
+	struct atomisp_frame_info output;	/* describe output frame */
+	struct ia_css_isp_parameters isp_params;
 };
