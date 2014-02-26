b2d294f7ea67f7bbc4199cd4297157e2cddee38b
Bin Han <bin.b.han@intel.com>
2014-02-26 10:34:08 +0800

    atomisp2: correct error code for hrt_isp_css_mm APIs
    BZ: 174852
    
    When the memory pointer arguments are NULL, these APIs should return -EFAULT
    which means bad address rather than -EINVAL.
    
    Change-Id: If429a789ad11a6b2d9d93cebb91ed5f54c441451
    Signed-off-by: Bin Han <bin.b.han@intel.com>

--- drivers/media/pci/atomisp2/atomisp_driver/hrt/hive_isp_css_mm_hrt.c
+++ drivers/media/pci/atomisp2/atomisp_driver/hrt/hive_isp_css_mm_hrt.c
@@ -40,21 +40,21 @@ int hrt_isp_css_mm_set(ia_css_ptr virt_addr, int c, size_t bytes)
 	if (virt_addr)
 		return hmm_set(virt_addr, c, bytes);
 
-	return -EINVAL;
+	return -EFAULT;
 }
 
 int hrt_isp_css_mm_load(ia_css_ptr virt_addr, void *data, size_t bytes)
 {
 	if (virt_addr && data)
 		return hmm_load(virt_addr, data, bytes);
-	return -EINVAL;
+	return -EFAULT;
 }
 
 int hrt_isp_css_mm_store(ia_css_ptr virt_addr, const void *data, size_t bytes)
 {
 	if (virt_addr && data)
 		return hmm_store(virt_addr, data, bytes);
-	return -EINVAL;
+	return -EFAULT;
 }
 
 void hrt_isp_css_mm_free(ia_css_ptr virt_addr)
